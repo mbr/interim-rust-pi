187f0979ba37593da09a23786f2acd81607a10f1
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-04 13:08:50 +0200

    Cleanup when packaging again.

--- package-rust.sh
+++ package-rust.sh
@@ -5,7 +5,7 @@ set -x
 
 BUILDDIR=pi-tools-rust_1.1.0-1
 
-mkdir -p ${BUILDDIR}/opt/pi-tools/
+rm -rf ${BUILDDIR}/opt/pi-tools/rust
 cp -r /opt/pi-tools/rust ${BUILDDIR}/opt/pi-tools/
 chmod -R a+rX ${BUILDDIR}
 fakeroot dpkg-deb --build $(basename ${BUILDDIR})
