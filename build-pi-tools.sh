5b8e8d406c305a8a525da26b6989f6774ab2dcc3
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-04 12:28:34 +0200

    Also include arm-bcm2708hardfp-linux-gnueabi in pi-tools.

--- build-pi-tools.sh
+++ build-pi-tools.sh
@@ -6,8 +6,11 @@ set -x
 BUILDDIR=pi-tools_1
 TOOLSDIR=tools/arm-bcm2708
 CCDEST=${BUILDDIR}/opt/pi-tools/gcc-linaro-arm-linux-gnueabihf-raspbian-x64
+SRDEST=${BUILDDIR}/opt/pi-tools/arm-bcm2708hardfp-linux-gnueabi
 
 mkdir -p $(dirname ${CCDEST})
+rm -rf ${CCDEST} ${SRDEST}
 cp -r ${TOOLSDIR}/gcc-linaro-arm-linux-gnueabihf-raspbian-x64 ${CCDEST}
+cp -r ${TOOLSDIR}/arm-bcm2708hardfp-linux-gnueabi ${SRDEST}
 chmod -R a+rX ${BUILDDIR}
 fakeroot dpkg-deb --build $(basename ${BUILDDIR})
